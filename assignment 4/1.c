ae9d6d778d7559e562142e4d4bef3959c78934ba
RAJ ROY <119309033+RAJ8664@users.noreply.github.com>
2022-12-13 22:12:55 +0530

    Update 1.c

--- assignment 4/1.c
+++ assignment 4/1.c
@@ -1,13 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-void printarray(int arr[],int n){
-    for(int i=0;i<n;i++){
-        printf("%d ",arr[i]);
-    }
-}
 int main(){
-    int arr[]={1,2,3,4,5};
-    //to find size of array
-    int n=sizeof(arr)/sizeof(arr[0]);
-    printarray(arr,n);
+    int arr1[100], n,ctr=0;
+    int i, j, k;
+       printf("\n\nPrint all unique elements of an array:\n");
+       printf("------------------------------------------\n");
+       printf("Input the number of elements to be stored in the array: ");
+       scanf("%d",&n);
+       printf("Input %d elements in the array :\n",n);
+       for(i=0;i<n;i++)
+            {
+	      printf("element - %d : ",i);
+	      scanf("%d",&arr1[i]);
+	    }
+    printf("\nThe unique elements found in the array are: \n");
+    for(i=0; i<n; i++)
+    {
+        ctr=0;
+        for(j=0,k=n; j<k+1; j++)
+        {
+            /*Increment the counter when the seaarch value is duplicate.*/
+            if (i!=j)
+            {
+		       if(arr1[i]==arr1[j])
+              {
+                 ctr++;
+               }
+             }
+        }
+       if(ctr==0)
+        {
+          printf("%d ",arr1[i]);
+        }
+    }
+       printf("\n\n");
 }
